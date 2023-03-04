49e95be66466729413004fb7105c3c06e8fcaa54
ruesei-koseki <51506011+ruesei-koseki@users.noreply.github.com>
2023-03-04 15:08:28 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,4 @@
-sudo apt install -y python3
-sudo apt install -y pip
-sudo apt install -y portaudio19-dev
-sudo pip3 install mutagen
-sudo pip3 install pygame
-sudo pip3 install bs4
-sudo pip3 install SpeechRecognition
-sudo pip3 install pyaudio
-sudo apt install -y ffmpeg
-sudo pip3 install pydub
 sudo apt-get install -y -qq build-essential swig git libpulse-dev libasound2-dev -y
 sudo apt-get install -y ruby-dev g++ make ruby -y
 sudo gem install discordrb
 sudo gem install suika
-sudo gem install pycall
-
